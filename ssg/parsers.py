fb7c3217b80dffec96781a937143330db4a733ab
FeatherCat <30635065+FeatherCat@users.noreply.github.com>
2022-02-04 15:52:31 -0800

    more fixes

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,8 +6,7 @@ import shutil
 class Parser:
     extensions: List[str] = []
     def valid_extension(self, extension):
-        if extension in self.extensions:
-            return True
+        return extension in self.extensions
 
     def parse(self, path, source, dest) -> Path:
         raise NotImplementedError
