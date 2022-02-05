9300c1e9226db44d5b5d6075475f36e3ab2bb0df
FeatherCat <30635065+FeatherCat@users.noreply.github.com>
2022-02-04 16:00:24 -0800

    fixes

--- ssg/site.py
+++ ssg/site.py
@@ -5,7 +5,7 @@ class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-        parsers = parsers or []
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest/Path.relative_to(self.source)
@@ -21,7 +21,7 @@ class Site:
 
     def load_parser(self, extension):
         for parser in self.parsers:
-            if Parser.valid_extension(parser):
+            if Parser.valid_extension(extension):
                 return parser
 
     def run_parser(self, path):
